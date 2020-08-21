4dff556459961713e27453afc3d7f381af8e7820
Gouda_Clark <mclarkuga@gmail.com>
2020-08-20 22:01:28 -0400

    added email functionality

--- planner/__init__.py
+++ planner/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from planner.pw import email_password, email_username
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '10eba29c88131580215be7db2c58c78c'
@@ -16,8 +17,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = ""
-app.config['MAIL_PASSWORD'] = ""
+app.config['MAIL_USERNAME'] = email_username
+app.config['MAIL_PASSWORD'] = email_password
 mail = Mail(app)
 
 
